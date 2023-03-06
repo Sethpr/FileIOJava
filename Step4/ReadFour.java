5b7ccfac0accf735aef0e0572f5c27b99dd422cc
Seth <seth@Zipcoders-MacBook-Pro.local>
2023-03-06 11:15:53 -0500

    added pom and filled in step 4, also added a bunch of out files and im not sure what they are, will figure out and fix if a problem

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,5 +1,6 @@
 package Step4;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -10,8 +11,20 @@ public class ReadFour
      * Yes, you can look at the other 
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
+        public static void main(String[] args) {
+            int sum = 0;
+            try {
+                Scanner fileIn = new Scanner(new File("Step4/input.txt"));
+
+                while (fileIn.hasNext()) {
+                    String lineIn = fileIn.nextLine();
+                    sum += Integer.parseInt(lineIn);
+
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(sum);
             // Print out a running total of all the
             // values in the input file.
         }
